2ba18eaee7cf04f8eef69e007fd82c5a0c3c2c61
davidlasry <lasry.david@gmail.com>
2021-07-12 02:33:13 +0300

    first commit

---
+++ src/data/table-columns.ts
@@ -0,0 +1,50 @@
+export const TABLE_COLUMNS = [
+  {
+    name: "food",
+    label: "Food",
+    options: {
+      filter: true,
+      sort: true,
+    },
+  },
+  {
+    name: "kCal",
+    label: "Kcal",
+    options: {
+      filter: true,
+      sort: false,
+    },
+  },
+  {
+    name: "protein",
+    label: "Prot.",
+    options: {
+      filter: true,
+      sort: false,
+    },
+  },
+  {
+    name: "lipids",
+    label: "Lip.",
+    options: {
+      filter: true,
+      sort: false,
+    },
+  },
+  {
+    name: "carbs",
+    label: "Carb.",
+    options: {
+      filter: true,
+      sort: false,
+    },
+  },
+  {
+    name: "water",
+    label: "Water",
+    options: {
+      filter: true,
+      sort: false,
+    },
+  },
+];
